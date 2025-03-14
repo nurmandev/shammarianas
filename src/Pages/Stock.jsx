0430ea8a5b138ddc597ef10132c192a0c0b688fb
nurmandev <adebayour66265@gmail.com>
2025-03-14 11:55:33 +0100

    add stock page

--- src/Pages/Stock.jsx
+++ src/Pages/Stock.jsx
@@ -1,34 +1,34 @@
-import React, { useEffect, useRef } from "react";
-import AOS from "aos"; // Import AOS (Animate On Scroll) library
-import "aos/dist/aos.css"; // Import AOS styles
-import image from "../assets/Images/b8.jpg";
-import Lines from "../Components/Lines";
-import ProgressScroll from "../common/ProgressScroll";
-import Cursor from "../common/cusor";
-import Marq2 from "../Components/marq2";
-import Header from "../Components/stock/Header";
-import ProductList from "../Components/stock/ProductList";
+// import React, { useEffect, useRef } from "react";
+// import AOS from "aos"; // Import AOS (Animate On Scroll) library
+// import "aos/dist/aos.css"; // Import AOS styles
+// import image from "../assets/Images/b8.jpg";
+// import Lines from "../Components/Lines";
+// import ProgressScroll from "../common/ProgressScroll";
+// import Cursor from "../common/cusor";
+// import Marq2 from "../Components/marq2";
+// import Header from "../Components/stock/Header";
+// import ProductList from "../Components/stock/ProductList";
 
-export default function StockPage() {
-  const main = useRef();
-  // useEffect hook to initialize AOS and refresh it
-  useEffect(() => {
-    AOS.init({
-      once: true, // Set animation to occur only once
-      duration: 300, // Set animation duration to 300 milliseconds
-    });
-    AOS.refresh(); // Refresh AOS
-  }, []);
+// export default function StockPage() {
+//   const main = useRef();
+//   // useEffect hook to initialize AOS and refresh it
+//   useEffect(() => {
+//     AOS.init({
+//       once: true, // Set animation to occur only once
+//       duration: 300, // Set animation duration to 300 milliseconds
+//     });
+//     AOS.refresh(); // Refresh AOS
+//   }, []);
 
-  return (
-    <>
-      <Cursor />
-      <ProgressScroll />
-      <Lines />
-      <Header />
-      <ProductList />
+//   return (
+//     <>
+//       <Cursor />
+//       <ProgressScroll />
+//       <Lines />
+//       <Header />
+//       <ProductList />
 
-      <Marq2 />
-    </>
-  );
-}
+//       <Marq2 />
+//     </>
+//   );
+// }
