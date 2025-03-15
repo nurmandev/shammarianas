99886772837179b162827576b8d981b686ea3908
Ekaji <ekajionah@gmail.com>
2025-03-15 13:30:16 +0100

    fixed upload not displaying

--- src/Components/DescriptionBox.jsx
+++ src/Components/DescriptionBox.jsx
@@ -76,7 +76,7 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import remarkGfm from "remark-gfm";
 
-const DescriptionBox = ({ name, value, onChange }) => {
+const DescriptionBox = ({ name, value = "", onChange }) => {
   const renderers = {
     code: ({ node, inline, className, children, ...props }) => {
       const match = /language-(\w+)/.exec(className || "");
