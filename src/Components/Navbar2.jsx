7c01d78560ac8a059996eb22996e00c4607906ee
nurmandev <adebayour66265@gmail.com>
2025-03-28 20:23:18 +0100

    update navbar

--- src/Components/Navbar2.jsx
+++ src/Components/Navbar2.jsx
@@ -63,151 +63,226 @@ const Navbar = () => {
   }
   // If user is NOT logged in
   if (!currentUser) {
-    
     return (
-      <nav className="navbar navbar-expand-lg bord blur">
-        <div className="container o-hidden">
-          <a className="logo icon-img-100" href="#">
-            <img src="/assets/imgs/logo.png" className="logo" alt="logo" />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            onClick={() =>
-              document
-                .querySelector(".navbar .navbar-collapse")
-                ?.classList.toggle("show")
-            }
-          >
-            <span className="icon-bar">
-              <i className="fas fa-bars"></i>
-            </span>
-          </button>
-          <div
-            className="collapse navbar-collapse justify-content-center"
-            id="navbarSupportedContent"
-          >
-            <ul className="navbar-nav">
-              <li
-                onMouseLeave={handleDropdownMouseLeave}
-                onMouseMove={handleDropdownMouseMove}
-                className="nav-item dropdown"
-              >
-                <a
-                  className="nav-link"
-                  href="#"
-                  role="button"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <span className="rolling-text">Home</span>
-                </a>
-              </li>
-              <li
-                onMouseLeave={handleDropdownMouseLeave}
-                onMouseMove={handleDropdownMouseMove}
-                className="nav-item dropdown"
-              >
-                <a
-                  className="nav-link dropdown-toggle"
-                  data-toggle="dropdown"
-                  href="#"
-                  role="button"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <span className="rolling-text">Services</span>
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#hot">
-                      Hot
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#Printable">
-                      Printable
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#Models">
-                      Models
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#Textures">
-                      Textures
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#Scripts">
-                      Scripts
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#Shaders">
-                      Shaders
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#Plugins">
-                      Plugins
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#HDRIs">
-                      HDRIs
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li
-                onMouseLeave={handleDropdownMouseLeave}
-                onMouseMove={handleDropdownMouseMove}
-                className="nav-item dropdown"
-              >
-                <a
-                  className="nav-link"
-                  href="#portfolio"
-                  role="button"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <span className="rolling-text">Portfolio</span>
-                </a>
-              </li>
-              <li
-                onMouseLeave={handleDropdownMouseLeave}
-                onMouseMove={handleDropdownMouseMove}
-                className="nav-item dropdown"
-              >
-                <a
-                  className="nav-link"
-                  href="#blog"
-                  role="button"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <span className="rolling-text">Blogs</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#contact">
-                  <span className="rolling-text">Contact Us</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="contact-button">
-            <Link
-              to="/login"
-              className="butn butn-sm butn-bg main-colorbg radius-5"
+      <>
+        <nav className="navbar navbar-expand-lg bord blur">
+          <div className="container o-hidden">
+            <a className="logo icon-img-100" href="#">
+              <img src="/assets/imgs/logo.png" className="logo" alt="logo" />
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              onClick={() =>
+                document
+                  .querySelector(".navbar .navbar-collapse")
+                  ?.classList.toggle("show")
+              }
             >
-              <span className="text">Sign In</span>
-            </Link>
+              <span className="icon-bar">
+                <i className="fas fa-bars"></i>
+              </span>
+            </button>
+            <div
+              className="collapse navbar-collapse justify-content-center"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav">
+                <li
+                  onMouseLeave={handleDropdownMouseLeave}
+                  onMouseMove={handleDropdownMouseMove}
+                  className="nav-item dropdown"
+                >
+                  <a
+                    className="nav-link"
+                    href="#"
+                    role="button"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <span className="rolling-text">Home</span>
+                  </a>
+                </li>
+                <li
+                  onMouseLeave={handleDropdownMouseLeave}
+                  onMouseMove={handleDropdownMouseMove}
+                  className="nav-item dropdown"
+                >
+                  <a
+                    className="nav-link"
+                    href="#stock"
+                    role="button"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <span className="rolling-text">Stock</span>
+                  </a>
+                </li>
+                <li
+                  onMouseLeave={handleDropdownMouseLeave}
+                  onMouseMove={handleDropdownMouseMove}
+                  className="nav-item dropdown"
+                >
+                  <a
+                    className="nav-link"
+                    href="#services"
+                    role="button"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <span className="rolling-text">Services</span>
+                  </a>
+                </li>
+                {/* <li
+                  onMouseLeave={handleDropdownMouseLeave}
+                  onMouseMove={handleDropdownMouseMove}
+                  className="nav-item dropdown"
+                >
+                  <a
+                    className="nav-link dropdown-toggle"
+                    data-toggle="dropdown"
+                    href="#"
+                    role="button"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <span className="rolling-text">Services</span>
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#hot">
+                        Hot
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#Printable">
+                        Printable
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#Models">
+                        Models
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#Textures">
+                        Textures
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#Scripts">
+                        Scripts
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#Shaders">
+                        Shaders
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#Plugins">
+                        Plugins
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#HDRIs">
+                        HDRIs
+                      </a>
+                    </li>
+                  </ul>
+                </li> */}
+                <li
+                  onMouseLeave={handleDropdownMouseLeave}
+                  onMouseMove={handleDropdownMouseMove}
+                  className="nav-item dropdown"
+                >
+                  <a
+                    className="nav-link"
+                    href="#portfolio"
+                    role="button"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <span className="rolling-text">Portfolio</span>
+                  </a>
+                </li>
+                <li
+                  onMouseLeave={handleDropdownMouseLeave}
+                  onMouseMove={handleDropdownMouseMove}
+                  className="nav-item dropdown"
+                >
+                  <a
+                    className="nav-link"
+                    href="#blog"
+                    role="button"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <span className="rolling-text">Blogs</span>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#contact">
+                    <span className="rolling-text">Contact Us</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="contact-button">
+              <Link
+                to="/login"
+                className="butn butn-sm butn-bg main-colorbg radius-5"
+              >
+                <span className="text">Sign In</span>
+              </Link>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+
+        {["/stock"].includes(currentPage) && (
+          <div className="bottom-bar">
+            <div className="links">
+              <ul>
+                {[
+                  { path: "/hot", label: "Hot" },
+                  { path: "/Videos", label: "Videos" },
+                  { path: "/Models", label: "3D Models" },
+                  { path: "/templates", label: "Video Template" },
+                  { path: "/images", label: "Pictures" },
+                  { path: "/graphics", label: "Graphic Templates" },
+                  { path: "/Mockups", label: "Mockups" },
+                  { path: "/Fonts", label: "Fonts" },
+                  { path: "/hot", label: "More" },
+                ].map(({ path, icon, label }) => (
+                  <li key={path}>
+                    <NavLink
+                      className={({ isActive }) =>
+                        isActive ? "active" : undefined
+                      }
+                      to={path}
+                    >
+                      <i className={`icon fa-solid ${icon}`}></i> {label}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="buttons">
+              {/* <Link to="/Upload">
+              <button>
+                <i className="icon fa-solid fa-plus"></i> Upload
+              </button>
+            </Link> */}
+
+              <Link to={currentUser?.uid ? "/Trade" : "/Login"}>
+                <button>
+                  <i className="icon fa-solid fa-right-left"></i> Trade
+                </button>
+              </Link>
+            </div>
+          </div>
+        )}
+      </>
     );
   }
 
