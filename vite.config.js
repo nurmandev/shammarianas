0a519abcedb24c35f07f08d81bce57d992429f0c
Asin-Junior-Honore <asinhonore823@gmail.com>
2025-04-22 12:07:28 +0100

    `Removed NodeModulesPolyfillPlugin and NodeGlobalsPolyfillPlugin from vite.config.js`

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+// import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+// import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -24,14 +24,14 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ["buffer"],
-    esbuildOptions: {
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
+    // esbuildOptions: {
+    //   plugins: [
+    //     NodeGlobalsPolyfillPlugin({
+    //       buffer: true,
+    //     }),
+    //     NodeModulesPolyfillPlugin(),
+    //   ],
+    // },
   },
   // resolve: {
   //   alias: {
