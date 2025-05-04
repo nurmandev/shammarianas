6a4d06f8a75f887040397dde06fc0544bd098e15
Adebayour66265 <adebayour66265@gmail.com>
2025-05-05 00:09:51 +0100

    update services images

--- src/Components/Branding/Creative.jsx
+++ src/Components/Branding/Creative.jsx
@@ -42,7 +42,7 @@ function Creativity() {
                 <img
                   src="/assets/imgs/Asset_img.png"
                   alt=""
-                  className="w-[80px] h-[50px] object-contain mx-auto"
+                  className="object-contain mx-auto"
                 />
               </div>
             </div>
