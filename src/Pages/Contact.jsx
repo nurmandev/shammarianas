7167546a66150cc6f94c5e06e774dc943a385d89
Adebayour66265 <adebayour66265@gmail.com>
2025-05-08 10:10:03 +0100

    update contact

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -131,7 +131,7 @@ function Contact() {
       <div className="google-map">
         <iframe
           id="gmap_canvas"
-          src="https://maps.google.com/maps?q=hollwood&t=&z=11&ie=UTF8&iwloc=&output=embed"
+          src="https://maps.google.com/maps?q=dubai&t=&z=11&ie=UTF8&iwloc=&output=embed"
         ></iframe>
       </div>
     </>
