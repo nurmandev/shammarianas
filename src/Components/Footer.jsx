44f78ae1b6f3496252ab1e280c5f71f638c57f39
Adebayour66265 <adebayour66265@gmail.com>
2025-05-09 09:00:42 +0100

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="clean-footer crev">
+    <footer className="clean-footer">
       <div className="container pb-40 pt-40 ontop">
         <div className="row justify-content-between">
           <div className="col-lg-2">
